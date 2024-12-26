bf2bb1ec5217da146849ac3e8e07eaa6a9555d13
Sayan Ghosh <sayanvk90@gmail.com>
2024-12-27 03:34:09 +0530

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const ws_1 = require("ws");
-const app = (0, express_1.default)();
-const server = app.listen(process.env.PORT || 8000);
-const wss = new ws_1.WebSocketServer({ server });
-wss.on('connection', (ws) => {
-    ws.on('error', err => console.log(err.message));
-    ws.on('message', (data, isBinary) => {
-        wss.clients.forEach((client) => {
-            if (client != ws && client.readyState == ws_1.WebSocket.OPEN)
-                client.send(data, { binary: isBinary });
-        });
-    });
-});
-
+import express from 'express'
+import { WebSocket, WebSocketServer } from 'ws';
+const app=express();
+const server=app.listen(process.env.PORT||8000)
+const wss=new WebSocketServer({server})
+wss.on('connection',(ws)=>{
+    ws.on('error',err=>console.log(err.message))
+    
+    ws.on('message',(data ,isBinary)=>{
+        wss.clients.forEach((client)=>{
+            if(client.readyState==WebSocket.OPEN)client.send(data,{binary:isBinary});
+        })
+    })
+})
