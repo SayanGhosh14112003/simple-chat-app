cfc0430b430afd4eb6a69f21f267938c71859a2a
Sayan Ghosh <sayanvk90@gmail.com>
2024-12-27 03:13:49 +0530

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import { WebSocket, WebSocketServer } from 'ws';
+const app=express();
+const server=app.listen(process.env.PORT||8000)
+const wss=new WebSocketServer({server})
+wss.on('connection',(ws)=>{
+    ws.on('error',err=>console.log(err.message))
+    
+    ws.on('message',(data ,isBinary)=>{
+        wss.clients.forEach((client)=>{
+            if(client!=ws && client.readyState==WebSocket.OPEN)client.send(data,{binary:isBinary});
+        })
+    })
+})
