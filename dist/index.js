56efb9fe9e76f9241f693598987162114a79c137
Sayan Ghosh <sayanvk90@gmail.com>
2024-12-27 03:30:42 +0530

    minor changes

--- dist/index.js
+++ dist/index.js
@@ -6,15 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ws_1 = require("ws");
 const app = (0, express_1.default)();
-const server = app.listen(8000, () => {
-    console.log('server started at port 8000');
-});
+const server = app.listen(process.env.PORT || 8000);
 const wss = new ws_1.WebSocketServer({ server });
 wss.on('connection', (ws) => {
     ws.on('error', err => console.log(err.message));
     ws.on('message', (data, isBinary) => {
         wss.clients.forEach((client) => {
-            if (client != ws && client.readyState == ws_1.WebSocket.OPEN)
+            if (client.readyState == ws_1.WebSocket.OPEN)
                 client.send(data, { binary: isBinary });
         });
     });
